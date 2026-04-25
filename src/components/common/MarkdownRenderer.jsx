d2b38120936a669462251e3bf3f39775205f3036
Anshul-Agarwal-8081 <loveagarwal885@gmail.com>
2026-04-25 21:37:40 +0530

    improved input

--- src/components/common/MarkdownRenderer.jsx
+++ src/components/common/MarkdownRenderer.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import './MarkdownRenderer.css';
 
 const MarkdownRenderer = ({ content }) => {
   return (
     <div className="markdown-content">
-      <p>{content}</p>
+      <ReactMarkdown
+        components={{
+          code({node, inline, className, children, ...props}) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                children={String(children).replace(/\n$/, '')}
+                style={vscDarkPlus}
+                language={match[1]}
+                PreTag="div"
+              />
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            )
+          }
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 };
